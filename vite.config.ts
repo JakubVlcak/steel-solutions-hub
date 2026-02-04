cab664f8e788c56be90854b3ab2a4b1186b14758
JV <jakub.vlcak888@gmail.com>
2026-02-04 08:46:59 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/worksteel2/",
+  base: "/steel-solutions-hub/",
   server: {
     host: "::",
     port: 8080,
